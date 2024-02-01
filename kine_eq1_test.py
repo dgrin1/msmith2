a5af85380c5b4b22309ae2f95f39d041ec4d34e7
marysmith <marysmith@marys-mbp-3.haverford.edu>
2024-02-01 12:16:42 -0500

    adding classwork equation

---
+++ kine_eq1_test.py
@@ -0,0 +1,9 @@
+### easy kinematics equation
+v0=input("initial velocity:")
+v0=int(v0)
+a=input("acceleration:")
+a=int(a)
+t=input("time:")
+t=int(t)
+velocity=v0+a*t
+print("the velocity is ",velocity)
