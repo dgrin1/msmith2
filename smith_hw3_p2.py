a66e6c35367b6a17402909dc187adad051fa8621
marysmith <masmith@haverford.edu>
2024-02-28 22:03:17 -0500

    finished - needs comments

--- smith_hw3_p2.py
+++ smith_hw3_p2.py
@@ -1,39 +1,20 @@
-"""import numpy as np
-import matplotlib.pyplot as plt
-def mad(n):
-    M=float(0)
-    for l in range(-n,n+1):
-        for k in range(-n,n+1):
-            for j in range(-n,n+1):
-                M+=((-1)**(j+k+l))/(np.sqrt(j**2+k**2+l**2))
-    return(M)
-Ms=[]
-n=range(0,100)
-for i in n:
-    Ms.append(mad(n))
-plt.plot(n,Ms)
-plt.show()"""
-
-"""import math
-
-nmax = 13
-M = 0.
-for n in range(1,nmax,2):
-    for m in range(n,nmax,2):
-        fac = 2
-        if n == m:
-            fac = 1
-        M += fac * math.cosh(0.5*math.pi*math.hypot(n, m))**-2
-print(-12*math.pi*M)"""
 import numpy as np
-nmax=15
+import matplotlib.pyplot as plt
 def mad(n):
     M=0.
     for l in range(-n,n+1):
         for k in range(-n,n+1):
             for j in range(-n,n+1):
-                M+=((-1)**(j+k+l))/(np.sqrt(j**2+k**2+l**2))
+                if l!=0 and k!=0 and j!=0:
+                    M+=((-1)**(j+k+l))/(np.sqrt(j**2+k**2+l**2))
+                if l==0 and k==0 and j==0:
+                    pass
     return M
-print (mad(5))
+Ms=[]
+x=range(0,100)
+for n in x:
+    Ms.append(mad(n))
+plt.plot(x,Ms)
+plt.show()
         
         
